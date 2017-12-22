cc108fc0649635e2cab900438c8ca8a2f883289d
digitaljosh <joshuajmarkus@gmail.com>
2017-12-22 14:26:24 -0800

    Solution day 2 part 1

---
+++ Day2.py
@@ -0,0 +1,17 @@
+import csv;
+
+
+
+file_reader= open('Day2data.csv');
+read = csv.reader(file_reader, delimiter='\t');
+checksum = 0;
+for row in read:
+    list = [];
+    for cell in row:
+        list.append(int(cell));
+    difference = max(list)-min(list);
+    checksum+=difference;
+        
+print(checksum);
+
+        
