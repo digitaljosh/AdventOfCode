e90b7844ba27ec67789ddda38c154e25cd88b13d
digitaljosh <joshuajmarkus@gmail.com>
2017-12-26 11:58:07 -0800

    update

--- Day2/Day2Part2.py
+++ Day2/Day2Part2.py
@@ -1,7 +1,5 @@
 import csv;
 
-
-
 file_reader= open('Day2data.csv');
 read = csv.reader(file_reader, delimiter='\t');
 checksum = 0;
@@ -13,9 +11,7 @@ for row in read:
             if cell_b == cell_a:
                 pass;
             elif cell_b % cell_a == 0:
-                print(cell_a, cell_b);
                 result = cell_b/cell_a;
-                print(result);
                 checksum+=result;
        
         
